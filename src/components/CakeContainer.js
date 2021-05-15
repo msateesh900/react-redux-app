cd53a8c32d6a61c7fa2037c0cedaa215229a51b1
Sateesh <sateeshnaidu900@gmail.com>
2021-05-15 19:57:41 +0530

    Added basic project structure for sample react redux application for cakeshop

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <div>
+      <p>Number of cakes</p>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
+
+export default CakeContainer;
