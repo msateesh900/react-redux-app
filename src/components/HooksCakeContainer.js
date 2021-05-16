c834fa311de5c52ccd0dcb55021cc312fbd83dd9
Sateesh <sateeshnaidu900@gmail.com>
2021-05-16 19:58:43 +0530

    Added useSelector,useDispatch and HookscakContainer component in react-redux

---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../redux";
+
+function HooksCakeContainer() {
+  const numberOfCakes = useSelector((state) => state.numberOfCakes);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <p>Num of cakes - {numberOfCakes}</p>
+      <button
+        onClick={() => {
+          dispatch(buyCake());
+        }}
+      >
+        Buy cake
+      </button>
+    </div>
+  );
+}
+
+export default HooksCakeContainer;
