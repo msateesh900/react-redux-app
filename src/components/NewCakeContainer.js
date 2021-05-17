8e250bd5e5cc228eb54add52b01a992bd8efcbc6
Sateesh <sateeshnaidu900@gmail.com>
2021-05-17 17:07:50 +0530

    Added action payload to cake container and added input parameters for buy cake

---
+++ src/components/NewCakeContainer.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
+
+function NewCakeContainer(props) {
+  const [number, setNumber] = useState(1);
+  return (
+    <div>
+      <p>Number of cakes - {props.numberOfCakes}</p>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      &nbsp;
+      <button onClick={() => props.buyCake(number)}>Buy{number} Cake</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numberOfCakes: state.cake.numberOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: (number) => dispatch(buyCake(number)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer);
