55b6fbefbd4e5219fee0256f3428933b729bcb5c
Sateesh <sateeshnaidu900@gmail.com>
2021-05-17 16:02:14 +0530

    Added redux-devtools-extension to the application to debug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 // import cakeReducer from "./cake/cakeReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
